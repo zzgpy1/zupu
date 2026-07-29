70ef4a8cb81957b2f335c1facce0a532640db063
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-29 11:04:37 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {},
+  images: {
+    domains: ['your-r2-domain.com'], // 替换为你的 R2 公开域名
+  },
+};
+
+module.exports = nextConfig;
