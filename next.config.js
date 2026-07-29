caddf420a24e4688b7e8ca92e3fbfc2925e39cc5
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-29 13:42:50 +0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {},
-  images: {
-    domains: ['your-r2-domain.com'], // 替换为你的 R2 公开域名
-  },
-};
-
-module.exports = nextConfig;
